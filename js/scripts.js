e781afbf7db552a14d50a992c195c65a345e955a
korahschleinkofer <75759910+korahschleinkofer@users.noreply.github.com>
2021-01-27 14:36:16 -0800

    Randomize Background Color -numRand

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,13 @@
 console.log("ok");
+const background = ["bg0", "bg1", "bg2"];
+
+// Get random Number
+var numRand;
+var max = 3;
+numRand = Math.floor(Math.random() * max);
+
+//Get Element
+let pageBackground = document.querySelector("body");
+
+//set class name from stored values
+pageBackground.className = background[numRand];
